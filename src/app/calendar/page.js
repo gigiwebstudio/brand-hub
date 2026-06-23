6815fe13d48c2b0b4764a7928fa15b946fd23f45
gigiwebstudio <gigiweb.studio@gmail.com>
2026-06-23 12:29:13 -0500

    Update page.js

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,4 +1,10 @@
-import ContentCalendar from '../../components/ContentCalendar'
+'use client'
+import dynamic from 'next/dynamic'
+
+const ContentCalendar = dynamic(
+  () => import('../../components/ContentCalendar'),
+  { ssr: false }
+)
 
 export default function CalendarPage() {
   return <ContentCalendar />
