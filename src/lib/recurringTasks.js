55d39851fb41353e24fc5fac9cabf2089a8438b0
gigiwebstudio <gigiweb.studio@gmail.com>
2026-08-31 12:45:08 -0300

    Update recurringTasks.js

--- src/lib/recurringTasks.js
+++ src/lib/recurringTasks.js
@@ -5,16 +5,29 @@ export const RECURRING_WEEKLY_TASKS = [
   {
     client: 'Joayo Therapy',
     taskTitle: '주간 인스타그램 디자인',
-    taskDescription: '이번 주 인스타그램 포스트 디자인 2장 제작',
+    taskDescription: '이번 주 인스타그램 포스트 디자인 3장 제작',
+    links: ['https://canva.link/6528gixgieev5bb'],
+    assignedTo: '슬기',
   },
   {
     client: 'Joayo Pilates',
     taskTitle: '주간 인스타그램 디자인',
-    taskDescription: '이번 주 인스타그램 포스트 디자인 2장 제작',
+    taskDescription: '이번 주 인스타그램 포스트 디자인 3장 제작',
+    links: ['https://canva.link/v938usoguvs4bee'],
+    assignedTo: '슬기',
   },
   {
     client: "Queen's Therapy",
     taskTitle: '주간 인스타그램 디자인',
-    taskDescription: '이번 주 인스타그램 포스트 디자인 2장 제작',
+    taskDescription: '이번 주 인스타그램 포스트 디자인 3장 제작',
+    links: ['https://canva.link/v2w568yu0hdn5rb'],
+    assignedTo: '슬기',
+  },
+  {
+    client: 'WOLHA',
+    taskTitle: '주간 인스타그램 디자인',
+    taskDescription: '이번 주 인스타그램 포스트 디자인 3장 제작',
+    links: ['https://canva.link/gwjdqihade9ia43'],
+    assignedTo: '슬기',
   },
 ];
